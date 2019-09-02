0e518bf046f4d08ee3e0e713fe73304e804e786e
Haroldo Oliveira de Almeida Leite <holiverleite@gmail.com>
2019-09-02 13:15:08 -0300

    Initial Commit

---
+++ StrategyPatternExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StrategyPatternExample
+//
+//  Created by monitora on 02/09/19.
+//  Copyright © 2019 Haroldo Leite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
